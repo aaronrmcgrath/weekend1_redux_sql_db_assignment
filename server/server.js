7748878b8dd9c0116c31851b6a4247f8a4c1bd7d
Mbob023 <armcgrathmedia@gmail.com>
2016-03-17 22:03:20 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var index = require('./routes/index');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.set('port', (process.env.PORT || 4000));
+
+app.use('/', index);
+
+app.listen(app.get('port'), function(){
+    console.log('Listening on port: ', app.get('port'));
+});
